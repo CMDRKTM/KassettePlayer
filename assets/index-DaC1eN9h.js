f92165d5c22dc76b19d628b35ba0a8c7ced2431e
CMDRKTM <123240001+CMDRKTM@users.noreply.github.com>
2024-05-31 00:12:27 +0800

    Update index-DaC1eN9h.js

--- assets/index-DaC1eN9h.js
+++ assets/index-DaC1eN9h.js
@@ -32672,6 +32672,48 @@ function useDisclosure(props = {}) {
     })
   };
 }
+const EditIcon = ({
+  size = 30,
+  width,
+  height,
+  strokeWidth = 1.5,
+  fill = "none",
+  ...props
+}) => /* @__PURE__ */ jsxRuntimeExports.jsx(
+  "div",
+  {
+    style: {
+      display: "inline-block",
+      // or block, depending on your needs
+      position: "relative",
+      // this allows you to use top, right, bottom, left properties
+      top: "1px",
+      // adjust these values to position the SVG
+      left: "3.9px"
+      // you can also use margin or padding to position the SVG
+    },
+    children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+      "svg",
+      {
+        width: "30px",
+        height: "30px",
+        viewBox: "0 0 30 30",
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg",
+        ...props,
+        children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+          "path",
+          {
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            d: "M21.1213 2.70705C19.9497 1.53548 18.0503 1.53547 16.8787 2.70705L15.1989 4.38685L7.29289 12.2928C7.16473 12.421 7.07382 12.5816 7.02986 12.7574L6.02986 16.7574C5.94466 17.0982 6.04451 17.4587 6.29289 17.707C6.54127 17.9554 6.90176 18.0553 7.24254 17.9701L11.2425 16.9701C11.4184 16.9261 11.5789 16.8352 11.7071 16.707L19.5556 8.85857L21.2929 7.12126C22.4645 5.94969 22.4645 4.05019 21.2929 2.87862L21.1213 2.70705ZM18.2929 4.12126C18.6834 3.73074 19.3166 3.73074 19.7071 4.12126L19.8787 4.29283C20.2692 4.68336 20.2692 5.31653 19.8787 5.70705L18.8622 6.72357L17.3068 5.10738L18.2929 4.12126ZM15.8923 6.52185L17.4477 8.13804L10.4888 15.097L8.37437 15.6256L8.90296 13.5112L15.8923 6.52185ZM4 7.99994C4 7.44766 4.44772 6.99994 5 6.99994H10C10.5523 6.99994 11 6.55223 11 5.99994C11 5.44766 10.5523 4.99994 10 4.99994H5C3.34315 4.99994 2 6.34309 2 7.99994V18.9999C2 20.6568 3.34315 21.9999 5 21.9999H16C17.6569 21.9999 19 20.6568 19 18.9999V13.9999C19 13.4477 18.5523 12.9999 18 12.9999C17.4477 12.9999 17 13.4477 17 13.9999V18.9999C17 19.5522 16.5523 19.9999 16 19.9999H5C4.44772 19.9999 4 19.5522 4 18.9999V7.99994Z",
+            fill: "#000000"
+          }
+        )
+      }
+    )
+  }
+);
 function App() {
   const [editAlbumIndex, setEditAlbumIndex] = reactExports.useState(null);
   const [scrollBehavior, setScrollBehavior] = React.useState("inside");
@@ -32870,7 +32912,7 @@ function App() {
     setIsPlaybackComplete(false);
   };
   const extractAudioFilesFromZip = async (zipFile) => {
-    const JSZip = await __vitePreload(() => import("./jszip.min-CiP1pSpX.js").then((n2) => n2.j), true ? [] : void 0, import.meta.url);
+    const JSZip = await __vitePreload(() => import("./jszip.min-CwgHRpO-.js").then((n2) => n2.j), true ? [] : void 0, import.meta.url);
     const zip = await JSZip.loadAsync(zipFile);
     const audioFiles2 = [];
     await Promise.all(
@@ -33244,10 +33286,12 @@ function App() {
         /* @__PURE__ */ jsxRuntimeExports.jsx(
           button_default,
           {
-            color: "secondary",
-            className: "ml-0",
+            isIconOnly: true,
+            className: "text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-0 translate-x-0",
+            radius: "full",
+            variant: "light",
             onPress: () => handleEditAlbum(index),
-            children: "Edit"
+            children: /* @__PURE__ */ jsxRuntimeExports.jsx(EditIcon, { style: { opacity: 0.5 } })
           }
         ),
         /* @__PURE__ */ jsxRuntimeExports.jsx(
